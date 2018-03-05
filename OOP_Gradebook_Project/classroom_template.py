2b6155cdabb118e9a69df954774ae7a5ff2e9ee0
Jayce Azua <jayce.azua@gmail.com>
2018-03-04 23:16:20 -0800

    3 more questions to go.

--- OOP_Gradebook_Project/classroom_template.py
+++ OOP_Gradebook_Project/classroom_template.py
@@ -75,42 +75,66 @@ class Classroom(object):
         self.teacher_name = teacher_name
         self.class_day_and_time = class_day_and_time
         self.roster = {}
-        self.next_available_student_number = 0
+        self.next_available_student_number = 1
 
     def enroll_student(self, student_name):
         # - Expects student_name as string.
         # - Creates a new student object.
-        student_ID = self.next_available_student_number + 1
+        student_ID = self.next_available_student_number
         # - Sets the new student's student_ID to self.next_available_student_number
         new_student = Student(student_name, student_ID)
         # - increments self.next_available_student_number by 1.
         self.next_available_student_number += 1
         # - adds to roster dictionary, with new student's student_ID as key and student object as value.
         self.roster[new_student.student_ID] = new_student
-        print("{} was enrolled successfully".format(new_student.name))
+        print("{} was enrolled successfully in {}.".format(new_student.name, self.class_name))
 
+# I created this one on my own :D
+    def withdraw_student(self, student_name):
+        # - searches roster for student with student_name.
+        for student in self.roster.values():
+        # if the parameter matches
+            if student.name is student_name:
+        # delete that student
+                del self.roster[student.student_ID]
+        # let the teacher know the student is not enrolled.
+            else:
+                print("{} is not enrolled in this {} class.".format(student_name, self.class_name))
 
     def add_assignment_for_student(self, student_name, assignment_name, grade):
         '''Adds an assignment and corresponding grade for an individual student.'''
         # - searches roster for student with student_name.
+        for student in self.roster.values():
+            if student.name is student_name:
+        # - calls student object's add_assignment() method with assignment_name and grade as inputs.
+                student.add_assignment(assignment_name, grade)
         #     - if student does not exist, prints and error message.
-        # - calls student object's add_assignment() method with assignment_name and grade
-        #     as inputs.
-        pass
+            else:
+                print("{} is not enrolled in this {} class.".format(student_name, self.class_name))
 
 
     def _is_valid_grade(self, grade):
-        pass
+        # look it up and did not cheat.
+        try:
+            valid_grade = float(grade)
+            if valid_grade >= 0:
+                return True
+        # there are different Error types this being the most accurate
+        except ValueError:
+            return False
 
     def add_assignment_for_class(self, assignment_name):
         # - Expects assignment_name as String.
         # - Loops through each student in self.roster.values().
+        for student in self.roster.values():
         # - For each student, prints message asking teacher to add grade for student.name
+            grade = ""
         # - Once user has entered input, validates input using _is_valid_grade() method.
+            while not self._is_valid_grade(grade):
         #     - if input is not valid, asks user again for input.
-        # - If user input is valid, calls student's add_assignment() method and continues
-        #     through loop.
-        pass
+                grade = float(input("Enter {}'s grade for assignment {} --> ".format(student.name, assignment_name)))
+        # - If user input is valid, calls student's add_assignment() method and continues through loop.
+            student.add_assignment(assignment_name, grade)
 
 
     def drop_assignment_for_student(self, student_name, assignment_name):
@@ -132,13 +156,16 @@ class Classroom(object):
     def get_student_GPA(self, student_name):
         # - Expects student_name as String.
         # - Searches self.roster for student with student_name.
+        for student in self.roster.values():
+            # - If found, returns that student's self.GPA attribute.
+            if student.name is student_name:
+                return student.GPA
         #     - If no student with that name, prints error message saying so.
-        # - If found, returns that student's self.GPA attribute.
-        pass
-
+            else:
+                print("{} is not enrolled in this {} class.".format(student_name, self.class_name))
 
     def get_class_average(self):
         # -  Expects no inputs.
-        # -  Iterates through all students in self.roster.values() and returns the average
-        #     of their GPA attributes.
+        # -  Iterates through all students in self.roster.values() and returns the average of their GPA attributes.
+
         pass
--- OOP_Gradebook_Project/test_classrooms.py
+++ OOP_Gradebook_Project/test_classrooms.py
@@ -6,10 +6,6 @@ def create_classroom():
     new_classroom = Classroom("Chemistry", "Dr. O'Niel", "Mon/Wed 2PM - 4PM PST")
     return new_classroom
 
-# def create_student():
-#     new_student = Student("Jayce Azua", 27)
-#     return new_student
-
 def test_create_classroom():
     new_classroom = create_classroom()
     new_classroom_2 = Classroom("Computer Science with Python", "Instructor Mike Kane", "Mon 10:30AM PST")
@@ -19,22 +15,63 @@ def test_create_classroom():
     assert new_classroom.teacher_name == "Dr. O'Niel"
     assert len(new_classroom_2.roster) == 0
     assert len(new_classroom.roster) != 1
-    assert new_classroom.next_available_student_number == 0
-    assert new_classroom_2.next_available_student_number != 1
+    assert new_classroom.next_available_student_number == 1
+    assert new_classroom_2.next_available_student_number != 0
     assert new_classroom.class_day_and_time == "Mon/Wed 2PM - 4PM PST"
     assert new_classroom_2.class_day_and_time == "Mon 10:30AM PST"
 
 def test_enroll_student():
     new_classroom = create_classroom()
-    assert new_classroom.next_available_student_number == 0
-    new_classroom.enroll_student("Josh")
     assert new_classroom.next_available_student_number == 1
+    new_classroom.enroll_student("Josh")
+    assert new_classroom.next_available_student_number == 2
     new_classroom.enroll_student("Roan")
     assert len(new_classroom.roster) == 2
     assert new_classroom.roster[1].name == "Josh"
     assert new_classroom.roster[2].name == "Roan"
-    assert new_classroom.roster[1].student_ID != 0
+    assert new_classroom.roster[1].student_ID != 2
     assert new_classroom.roster[2].student_ID == 2
     assert new_classroom.roster[1] != new_classroom.roster[2]
-    assert new_classroom.next_available_student_number == 2
-    assert 2 in new_classroom.roster
+    assert new_classroom.next_available_student_number == 3
+
+# I created this one on my own :D
+def test_withdraw_student():
+    new_classroom = create_classroom()
+    new_classroom.enroll_student("Roan")
+    assert len(new_classroom.roster) == 1
+    new_classroom.enroll_student("Josh")
+    new_classroom.withdraw_student("Roan")
+    assert len(new_classroom.roster) == 1
+    new_classroom.withdraw_student("Josh")
+    assert len(new_classroom.roster) == 0
+    assert new_classroom.withdraw_student("Roan") == None
+    assert new_classroom.withdraw_student("Josh") == None
+
+def test_add_assignment_for_student():
+    new_classroom = create_classroom()
+    new_classroom.enroll_student("Jayce")
+    student = new_classroom.roster[1]
+    assert student.name == "Jayce"
+    new_classroom.add_assignment_for_student("Jayce", "Research Paper", 98)
+    assert len(student.assignments) == 1
+    assert student.assignments["Research Paper"] == 98
+    assert new_classroom.add_assignment_for_student("Ryan", "Research Paper", 98) == None
+
+def test_get_student_GPA():
+    new_classroom = create_classroom()
+    student = Student("Jayce", 1)
+    new_classroom.roster[1] = student
+    new_classroom.add_assignment_for_student("Jayce", "Research Paper", 98)
+    student.add_assignment("Atoms and DNA", 91)
+    assert student.assignments["Atoms and DNA"] == 91
+    assert new_classroom.get_student_GPA("Jayce") == 94.5
+
+def test_add_assignment_for_class():
+    classroom = create_classroom()
+    student_1 = Student("Jayce", 1)
+    student_2 = Student("Katherine", 2)
+    classroom.roster[1] = student_1
+    classroom.roster[2] = student_2
+    classroom.add_assignment_for_class("Sex Education")
+    assert student_1.assignments["Sex Education"] == 98.7
+    assert student_2.assignments["Sex Education"] == 78.3
